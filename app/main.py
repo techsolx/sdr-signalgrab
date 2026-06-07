5293d8295c2e996228faa7a34a46bc4a78f285f6
John MacTavish <john@techsolx.com>
2026-06-07 15:10:14 -0400

    wip(): unable to get direct sample rate 'Q'.

--- app/main.py
+++ app/main.py
@@ -6,15 +6,16 @@ sdr = rtlsdr.RtlSdr()
 
 # configure device
 sdr.sample_rate = 2.4e6
-sdr.center_freq = 1.5e6
-sdr.gain = 4
+sdr.gain = "auto"
+sdr.direct_sampling_mode = "0"
+sdr.center_freq = 127e6
 
-samples = sdr.read_samples(256*1024)
+samples = sdr.read_samples(256 * 1024)
 sdr.close()
 
 # use matplotlib to estimate and plot the PSD
-plt.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-plt.xlabel('Frequency (MHz)')
-plt.ylabel('Relative power (dB)')
+plt.psd(samples, NFFT=1024, Fs=sdr.sample_rate / 1e6, Fc=sdr.center_freq / 1e6)
+plt.xlabel("Frequency (MHz)")
+plt.ylabel("Relative power (dB)")
 
 plt.show()
